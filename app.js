6c45ab1e1c7ed7650ea5b9c9d48b8d6c327903c4
Simon Grigenas <grigenas@outlook.com>
2022-07-28 20:05:23 -0600

    Results Rendering Changes and Refactoring
    - Refactored axios to fetch for the API calls.
    - Segmented functions for rendering movie details and fetching movie details.
    - Added a basic README file.
    - Began work regarding the loading state. Loading icon and animation is complete.
    - Cleaned up other files/comments/code not being leveraged such as the JS TMDB Library.

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-// import * as theMovieDb from './themoviedb.js';
-// JS Library Import Not Working
-
-// function successCB(e) {
-// 	console.log('success', e);
-// }
-
-// function errorCB(e) {
-// 	console.log('fail', e);
-// }
-
-// theMovieDb.search.getMulti({ query: 'Game%20Of%20Thrones' }, successCB, errorCB);
-
 // API token access from config.
 const apiKey = config.MY_API_TOKEN;
 
@@ -32,13 +19,27 @@ const grid = document.querySelector('.grid');
 searchButton.addEventListener('click', (e) => {
 	// Prevent the form from refreshing the page.
 	e.preventDefault();
-	// Clear the results grid of movie art.
-	results.innerHTML = '';
-	// Clear the movie info and additional details.
-	clearCardInfo();
-	getSearchResults();
-	displayResults();
+	renderResults();
 });
+
+function renderResults() {
+	let loader = document.createElement('div');
+	loader.innerHTML = '<hr/><hr/><hr/><hr/>';
+	loader.classList.add('load');
+	grid.appendChild(loader);
+	try {
+		// Clear the results grid of movie art.
+		results.innerHTML = '';
+		// Clear the movie info and additional details.
+		clearCardInfo();
+		getSearchResults();
+		displayResults();
+	} catch (err) {
+		console.log(err);
+	} finally {
+		document.querySelector('.load').remove();
+	}
+}
 
 // Display results grid and scroll to it.
 function displayResults() {
@@ -54,93 +55,112 @@ function displayResults() {
 function getSearchResults() {
 	// Search field submitted text.
 	searchQuery = document.querySelector('.search-text').value;
-	axios
-		.get(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(searchQuery)}`)
-		.then((response) => {
-			response.data.results.forEach((movie, idx) => {
-				if (movie.id && movie['poster_path']) {
-					// Create div for card with Bootstrap classes required for formatting.
-					const cardDiv = document.createElement('div');
-					const cardClasses = [
-						'card',
-						'img-fluid',
-						'col-xs-1',
-						'col-m-3',
-						'd-flex',
-						'justify-content-center'
-					];
-					cardDiv.classList.add(...cardClasses);
 
-					// Create div for card-front with Bootstrap classes required for formatting and ID attribute.
-					const cardFrontDiv = document.createElement('div');
-					cardFrontDiv.classList.add('card-front');
-
-					// Capture the unique movie ID from search and add to card-front.
-					const movieID = movie.id;
-					cardFrontDiv.setAttribute('id', `${movieID}`);
-
-					// Create img to be hold movie artwork in results grid.
-					const imgArt = document.createElement('img');
-					const imgClasses = [
-						'movie-art',
-						'img-fluid'
-					];
-					imgArt.classList.add(...imgClasses);
-					imgArt.setAttribute('id', `${movieID}`);
-					imgArt.setAttribute(
-						'src',
-						`https://image.tmdb.org/t/p/w500/${response.data.results[idx]['poster_path']}`
-					);
-
-					// Create the card HTML element with all nested divs.
-					results.appendChild(cardDiv);
-					cardDiv.appendChild(cardFrontDiv);
-					cardFrontDiv.appendChild(imgArt);
-
-					// Show movie details once movie art is clicked.
-					const movieCover = document.getElementById(movieID);
-					movieCover.addEventListener('click', (e) => {
-						clearCardInfo();
-						getMovieDetails(movieID);
-						showCardInfo(e);
-					});
-				}
-			});
-		})
+	fetch(`https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${encodeURIComponent(searchQuery)}`)
+		.then(checkStatusAndParse)
+		.then(movieSearch)
 		.catch((err) => console.log(err));
+}
+
+// Fetch status check and JSON parse.
+function checkStatusAndParse(response) {
+	if (!response.ok) {
+		throw new Error(`Status Code Error: ${response.status}`);
+	}
+	return response.json();
+}
+
+// API search request for movie results grid.
+function movieSearch(response) {
+	response.results.forEach((movie, idx) => {
+		if (movie.id && movie['poster_path']) {
+			// Create div for card with Bootstrap classes required for formatting.
+			const cardDiv = document.createElement('div');
+			const cardClasses = [
+				'card',
+				'img-fluid',
+				'col-xs-1',
+				'col-m-3',
+				'd-flex',
+				'justify-content-center'
+			];
+			cardDiv.classList.add(...cardClasses);
+
+			// Create div for card-front with Bootstrap classes required for formatting and ID attribute.
+			const cardFrontDiv = document.createElement('div');
+			cardFrontDiv.classList.add('card-front');
+
+			// Capture the unique movie ID from search and add to card-front.
+			const movieID = movie.id;
+			cardFrontDiv.setAttribute('id', `${movieID}`);
+
+			// Create img to be hold movie artwork in results grid.
+			const imgArt = document.createElement('img');
+			const imgClasses = [
+				'movie-art',
+				'img-fluid'
+			];
+			imgArt.classList.add(...imgClasses);
+			imgArt.setAttribute('id', `${movieID}`);
+			imgArt.setAttribute('src', `https://image.tmdb.org/t/p/w500/${response.results[idx]['poster_path']}`);
+
+			// Create the card HTML element with all nested divs.
+			results.appendChild(cardDiv);
+			cardDiv.appendChild(cardFrontDiv);
+			cardFrontDiv.appendChild(imgArt);
+
+			// Show movie details once movie art is clicked.
+			const movieCover = document.getElementById(movieID);
+			movieCover.addEventListener('click', (e) => {
+				clearCardInfo();
+				getMovieDetails(movieID);
+				showCardInfo(e);
+			});
+		}
+	});
 }
 
 // Given the unique movieID search for and return the remaining details.
 function getMovieDetails(movieID) {
-	axios
-		.get(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}&language=en-US`)
+	fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=${apiKey}&language=en-US`)
+		.then(checkStatusAndParse)
 		.then((response) => {
-			// Update movie title.
-			movieTitle.innerHTML = '<b>Title: </b><br>' + response.data['title'];
-			// // Update movie release date.
-			releaseDate.innerHTML = '<b>Release Date: </b><br>' + response.data['release_date'];
-			// // Update movie description.
-			movieInfo.innerHTML = '<b>Description: </b><br>' + response.data['overview'];
+			const originalTitle = response['title'];
+			const date = response['release_date'];
+			const overview = response['overview'];
+
+			// Show the details of a selected movie artwork.
+			renderMovieDetails(originalTitle, overview, date);
 			// Update where you can stream, rent, or buy the movie.
 			getAvailableStreaming(movieID);
 		})
 		.catch((err) => console.log(err));
 }
 
+// Update movie card elements to include the selected movie artwork details.
+function renderMovieDetails(originalTitle, overview, date) {
+	// Update movie title.
+	movieTitle.innerHTML = '<b>Title: </b><br>' + originalTitle;
+	// // Update movie release date.
+	releaseDate.innerHTML = '<b>Release Date: </b><br>' + `${date}`;
+	// // Update movie description.
+	movieInfo.innerHTML = '<b>Description: </b><br>' + overview;
+}
+
 // Search for and append the list of watch providers where a movie is available to be streamed, rented, or bought.
 function getAvailableStreaming(movieID) {
-	axios
-		.get(`https://api.themoviedb.org/3/movie/${movieID}/watch/providers?api_key=${apiKey}`)
+	fetch(`https://api.themoviedb.org/3/movie/${movieID}/watch/providers?api_key=${apiKey}`)
+		.then(checkStatusAndParse)
 		.then((response) => {
 			// Checking if any services are available in Canada.
-			if (!response.data.results['CA']) {
+			if (!response.results['CA']) {
 				let div = document.createElement('div');
 				streamOn.innerHTML = '<br> <b>Not available to stream, rent, or buy in Canada.</b>';
 			} else {
 				// Checking if available with a subscription in Canada.
-				if (response.data.results['CA']['flatrate']) {
+				if (response.results['CA']['flatrate']) {
 					// Loop through subscription providers in Canada.
-					response.data.results['CA']['flatrate'].forEach((provider) => {
+					response.results['CA']['flatrate'].forEach((provider) => {
 						let li = document.createElement('li');
 						subList.appendChild(li).innerHTML = provider['provider_name'];
 					});
@@ -149,9 +169,9 @@ function getAvailableStreaming(movieID) {
 					subList.appendChild(li).innerHTML = 'Not available in Canada.';
 				}
 				// Checking if available for rent in Canada.
-				if (response.data.results['CA']['rent']) {
+				if (response.results['CA']['rent']) {
 					// Loop through rent providers in Canada.
-					response.data.results['CA']['rent'].forEach((provider) => {
+					response.results['CA']['rent'].forEach((provider) => {
 						let li = document.createElement('li');
 						rentList.appendChild(li).innerHTML = provider['provider_name'];
 					});
@@ -160,9 +180,9 @@ function getAvailableStreaming(movieID) {
 					rentList.appendChild(li).innerHTML = 'Not available in Canada.';
 				}
 				// Checking if available to buy in Canada.
-				if (response.data.results['CA']['buy']) {
+				if (response.results['CA']['buy']) {
 					// Loop through buy providers in Canada.
-					response.data.results['CA']['buy'].forEach((provider) => {
+					response.results['CA']['buy'].forEach((provider) => {
 						let li = document.createElement('li');
 						buyList.appendChild(li).innerHTML = provider['provider_name'];
 					});
